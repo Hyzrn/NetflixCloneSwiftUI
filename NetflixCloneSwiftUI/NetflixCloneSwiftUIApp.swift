255e1b6baf53eb2711c85ee2d903ec8df4b91581
shayzaran <shayzaran@gmail.com>
2021-09-04 14:51:19 +0300

    Initial Commit

---
+++ NetflixCloneSwiftUI/NetflixCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneSwiftUIApp.swift
+//  NetflixCloneSwiftUI
+//
+//  Created by Selahattin Hayzaran on 4.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
